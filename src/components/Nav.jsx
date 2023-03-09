65ef069c7b83b417997b65859a9d29d2da096d71
Mozibix <opuzmoses@gmail.com>
2023-03-09 16:11:15 +0100

    chores: adding links and  icons

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,24 @@
+import { Link } from "react-router-dom";
+import logo from "/logo.png";
+import whatsapp from "/whatsapp.gif";
+
 const Nav = () => {
   return (
     <>
       <nav>
-        <div className="container box_shadow">
-          <div className="logo fw_700">CocoAce Kollectibles</div>
+        <div className=" container flex_box box_shadow">
+          <div className="logo fw_700">
+            <img className="img" src={logo} alt="logo" />
+            CocoAce Kollectibles
+          </div>
+
+          <div className="social">
+            <div className="social_icon">
+              <Link to="https://api.whatsapp.com/send?phone=2349023019070&text=hello20%my20%name20%is ">
+                <img className="img" src={whatsapp} alt="whatsapp" />
+              </Link>
+            </div>
+          </div>
         </div>
       </nav>
     </>
